cf4e435235592d791e761bd9bd7a3b47820cc17a
tianboguan <122652151@qq.com>
2015-11-15 23:11:17 +0800

    redis 对接 protobuff测试能过

--- service/test/redis_test.cc
+++ service/test/redis_test.cc
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 
 #include "thirdparty/hiredis-master/hiredis.h"
+#include "proto/hello_proto/hello.pb.h"
 
+using namespace std;
 int main(int argc, char **argv) {
   // unsigned int j;
   redisContext *c;
   redisReply *reply;
 
-#if 0
-  if (argc < 4) {
-    printf("Usage: 192.168.0.195 6379 instance_id password\n");
-    exit(0);
-  }
-  const char *hostname = argv[1];
-  const int port = atoi(argv[2]);
-  const char *instance_id = argv[3];
-  const char *password    = argv[4];
-#endif
-
-
   struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-  // c = redisConnectWithTimeout(hostname, port, timeout);
   c = redisConnectWithTimeout("10.66.89.11", 6379, timeout);
   if (c == NULL || c->err) {
     if (c) {
@@ -45,14 +35,30 @@ int main(int argc, char **argv) {
   printf("PING: %s\n", reply->str);
   freeReplyObject(reply);
 
+  HelloMsg hello_msg;
+  hello_msg.set_hello("lalala we lalala");
+  hello_msg.set_standard_int(1);
+  hello_msg.set_long_int(123456789012);
+  // char buff[512];
+  // bzero(buff, sizeof(buff));
+  // const int byteSize = hello.ByteSize(); 
+  string serial_str;
+  hello_msg.SerializeToString(&serial_str);
+  cout << "msg: \n-------------------\n" << hello_msg.DebugString() << endl;
   /* Set a key */
-  reply = redisCommand(c,"SET %s %s", "name", "credis_test");
+  //reply = redisCommand(c,"SET %s %s", "name", "credis_test");
+  reply = redisCommand(c,"SET %b %b", "name", (size_t) 4, serial_str.data(), serial_str.size());
   printf("SET: %s\n", reply->str);
   freeReplyObject(reply);
 
   /* Try a GET  */
   reply = redisCommand(c,"GET name");
-  printf("GET name: %s\n", reply->str);
+  // printf("GET name: %s\n", reply->str);
+  // string test(reply->str, sizeof(buff));
+  cout << "GET name: " << reply->str << endl;
+  HelloMsg ret_msg;
+  ret_msg.ParseFromArray(reply->str, reply->len);
+  cout << "GET name: " << ret_msg.DebugString() << endl;
   freeReplyObject(reply);
 
   /* Disconnects and frees the context */
