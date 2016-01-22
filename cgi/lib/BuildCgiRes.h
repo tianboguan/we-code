982e01760a3bc56ba85ff0cc1f1e2e960aeaa4c9
tianboguan <122652151@qq.com>
2016-01-22 20:08:17 +0800

    update

--- cgi/lib/BuildCgiRes.h
+++ cgi/lib/BuildCgiRes.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <map>
 #include "common/pbjson/pbjson.h"
 #include "common/app/CgiCode.h"
 #include "thirdparty/plog/Log.h"
@@ -43,12 +44,15 @@ void SendPostResWithoutData(int code) {
 }
 
 void SendAddressRes(int code, const AddressRes &res) {
-  // string json;
-  // pbjson::pb2json(&data, json);
+  std::map<std::string, std::string> addresses;
+  ::google::protobuf::Map<::std::string, ::std::string>::const_iterator iter_pb;
+  for (iter_pb = res.address().begin(); iter_pb != res.address().end(); ++iter_pb) {
+    addresses[iter_pb->first] = iter_pb->second;
+  }
 
   string result;
-  ::google::protobuf::Map<::std::string, ::std::string>::const_iterator iter;
-  for (iter = res.address().begin(); iter != res.address().end(); ++iter) {
+  std::map<std::string, std::string>::iterator iter;
+  for (iter = addresses.begin(); iter != addresses.end(); ++iter) {
     result += "\"" + iter->first + "\":\"" + iter->second + "\"," ;
   } 
   // erase last ","
@@ -58,7 +62,6 @@ void SendAddressRes(int code, const AddressRes &res) {
   oss << "Content-type: text/script\n\n"
       << "{\"code\":" << code << ","
       << "\"message\":\"" << GetErrMsg(code) << "\","
-      // << "\"data\":" << json << "}\n";
       << "\"data\": {" << result << "}}\n";
 
   LOG_ERROR << "cgi response";
@@ -66,7 +69,6 @@ void SendAddressRes(int code, const AddressRes &res) {
   LOG_ERROR << oss.str();
 
   cout << oss.str();
-
 }
 
 #endif // CGI_LIB_BUILDCGIRES_H_
